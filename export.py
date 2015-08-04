c107732d0156d727b9e8bc987f08d6899dd544b7
Jakub Klinkovský <j.l.k@gmx.com>
2015-08-05 00:10:50 +0200

    clean up export.py

--- export.py
+++ export.py
@@ -1,4 +1,11 @@
-# export
+#!/usr/bin/env python
+
+import builtins
+import turtle as t
+
+import fractals._lib as _lib
+from fractals import *
+
 size = 1600
 border = 10
 color = "blue"
@@ -11,11 +18,11 @@ for i in range(0,17):
     penup()
     goto(-window_width()/2+size/4.1, window_height()/2-size*0.4)
     pendown()
-#~ #~
+
     dragon(i, size/1.55)
     update()
-#~ #~
+
     cv = getcanvas()
-    cv.postscript(file="/media/data/Bordýlek/Dračí křivka/dragon-"+color+"-"+str(i+1)+".ps", width=size+2*border, height=size+2*border)
-#~ #~
+    cv.postscript(file="dragon-"+color+"-"+str(i+1)+".ps", width=size+2*border, height=size+2*border)
+
     reset()
